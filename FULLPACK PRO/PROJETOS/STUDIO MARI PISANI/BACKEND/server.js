6786f692f491a6d12c6c61d7e394ef98fac29761
devmarcelosantana <dev.marcelosantanasf@gmail.com>
2026-09-13 21:44:43 -0300

    Update server.js

--- FULLPACK PRO/PROJETOS/STUDIO MARI PISANI/BACKEND/server.js
+++ FULLPACK PRO/PROJETOS/STUDIO MARI PISANI/BACKEND/server.js
@@ -641,8 +641,6 @@ app.get('/api/appointments/booked/:date', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`API do Studio Mari Pisani rodando na porta ${PORT}`);
 });
